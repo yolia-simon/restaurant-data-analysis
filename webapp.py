58de957fa2ad0c882007584ae76122ea8b693844
Yolia Simon <yolias628@gmail.com>
2025-12-30 23:20:20 +0000

    started on UI design 30/12/25

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from datetime import datetime
 import pandas as pd
 import math
@@ -10,8 +10,10 @@ df = dp.load_data()
 @app.route("/")
 def welcome():
   return render_template('base.html',
-                         message= f"Backend is working, last reload at {datetime.now().time()}"
+                         message= f"Backend is working, last reload at {datetime.now().time()}",
+                         image_url=url_for("static", filename="logo.png")
   )
+
 
 
 if __name__ == '__main__':
